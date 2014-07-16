1316e5402e29c387973c47fb5da2386783999979
Brian Cardarella <bcardarella@gmail.com>
2014-07-16 10:40:29 -0400

    EMBER_ADDON_ENV trees for building

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ function EmberWuphf(project) {
   this.name    = 'EmberWuphf';
 }
 
+function unwatchedTree(dir) {
+  return {
+    read:    function() { return dir; },
+    cleanup: function() { }
+  };
+}
+
 EmberWuphf.prototype.treeFor = function treeFor(name) {
   var treePath = path.relative(process.cwd(), __dirname);
 
@@ -18,7 +25,11 @@ EmberWuphf.prototype.treeFor = function treeFor(name) {
   }
 
   if (fs.existsSync(treePath)) {
-    return treePath;
+    if (process.env.EMBER_ADDON_ENV === 'development') {
+      return treePath;
+    } else {
+      return unwatchedTree(treePath);
+    }
   }
 };
 
