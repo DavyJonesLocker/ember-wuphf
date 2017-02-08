ec8108ff12f005643a79747ab10d2b09889329a1
David John <djohn@arch-no.org>
2017-02-08 12:52:17 -0600

    Bump to ember-cli 2.3.0

--- app/initializers/wuphf.js
+++ app/initializers/wuphf.js
@@ -3,7 +3,7 @@ import Wuphf from '../services/wuphf';
 export default {
   name: 'wuphf',
 
-  initialize: function(container, application) {
+  initialize: function(application) {
     application.register('wuphf:main', Wuphf);
     application.inject('controller', 'wuphf', 'wuphf:main');
     application.inject('component',  'wuphf', 'wuphf:main');
